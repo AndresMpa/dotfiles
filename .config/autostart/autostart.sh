57517999e053dee67f825a565660106754034a17
AndresMpa <andresprieto2703@gmail.com>
2021-02-11 17:00:42 -0500

    General changes

--- .config/autostart/autostart.sh
+++ .config/autostart/autostart.sh
@@ -3,6 +3,9 @@
 # Disable beep
 xset -b
 
+# Disable screelock (You may need to remove this)
+xset s off
+
 # Set up keymaps
 setxkbmap -layout latam
 
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,13 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using config location ~/.config/polybar/config
-# you have to invoce the bar like it was named in [bar/arch_bar]
-
-# Uncomment this and comment the next line if you want to make
-# changes in config
-
-# polybar -c ~/.config/polybar/config arch_bar -r &
-
+# Launch Polybar, using default config location ~/.config/polybar/config
 polybar -c ~/.config/polybar/config arch_bar &
 
+echo "Polybar launched..."
