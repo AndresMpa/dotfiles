9903454260b63f131df8eaa982f654aebeefde47
AndresMpa <andresprieto2703@gmail.com>
2021-02-11 17:00:42 -0500

    General changes

--- .config/autostart/wifi.sh
+++ .config/autostart/wifi.sh
@@ -1,7 +1,4 @@
-#!/bin/sh
-
-# May be neccessary for making wifi works
-
-nm-applet    > /dev/null 2>/dev/null &
-stalonetray  > /dev/null 2>/dev/null
-killall nm-applet
+ #!/bin/sh
+ nm-applet    > /dev/null 2>/dev/null &
+ stalonetray  > /dev/null 2>/dev/null
+ killall nm-applet
