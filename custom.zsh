da43b59027f40b07c5184cac03cf87384d588416
AndresMpa <andresprieto2703@gmail.com>
2021-03-23 12:54:18 -0500

    I made a couple of scripts to make this thing easier to update

--- custom.zsh
+++ custom.zsh
@@ -1,4 +1,24 @@
-# Paste this file to you oh-my-zsh/custom/example.zsh
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
 
 alias v=nvim
 alias wifi='sudo wifi-menu'
+
+info="
+________________________________________________
+\n\tUse feh to see images
+\n\tUse figlet to make custom text
+\n\tUse lolcat to custom the outputs
+\n\tUse cf + tab to see the configuration files
+_________________________________________________
+"
+
+alias help='echo -e $info'
