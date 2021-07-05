1a8cb9c2db4d89dc888a76a64590476647fee55b
AndresMpa <andresprieto2703@gmail.com>
2021-07-05 00:31:31 -0500

    Adding my pacman config file

--- .config/updateConfigFiles.sh
+++ .config/updateConfigFiles.sh
@@ -5,5 +5,8 @@ rm -rf alacritty/ bspwm/ nitrogen/ picom/ polybar/ rofi/ script/ sxhkd/ &&
 # Brigthness service
 rm -rf ./brightness-permission.service &&
 cp /etc/systemd/system/brightness-permission.service ./.config &&
+# Updating pacman configurations
+rm -rf ./pacman.conf
+cp /etc/pacman.conf ./.config
 # Configuration folders
 cp -r ~/.config/alacritty ~/.config/bspwm ~/.config/nitrogen/ ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/scripts ~/.config/sxhkd ./.config
