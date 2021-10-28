4a002275f2415d9b7b4d617924bc2939f426d52b
AndresMpa <andresprieto2703@gmail.com>
2021-10-28 07:50:37 -0500

    Adding Qtile config

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Setting default background 
+feh --bg-scale /home/andresmpa/Wallpapers/blue.png &
+
+#Setting my layout
+setxkbmap latam & 
+
+#NetworkManager service as demon
+m-applet &
