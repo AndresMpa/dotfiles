4925a037653c5e8e792749cec888098db941d1dc
AndresMpa <andresprieto2703@gmail.com>
2022-02-05 23:55:04 -0500

    I finally split my config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,4 +1,3 @@
-from libqtile.utils import guess_terminal
 from libqtile.config import Key
 from libqtile.lazy import lazy
 
@@ -13,74 +12,11 @@ from libqtile.config import Screen
 from libqtile import widget
 from libqtile import bar
 
+from setting.keys import keysMapping
 from util.color import getTheme
 
-terminal = guess_terminal()
 mod = "mod4"
-
-keys = [
-    # Switch between windows
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod, "control"], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack"),
-
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-
-    # Volume
-    Key([], "XF86AudioLowerVolume", lazy.spawn("pamixer --decrease 5")),
-    Key([], "XF86AudioRaiseVolume", lazy.spawn("pamixer --increase 5")),
-    Key([], "XF86AudioMute", lazy.spawn("pamixer --toggle-mute")),
-
-    # Brightness
-    Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +10%")),
-    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10%-")),
-
-    # Print Screen
-    Key([], "Print", lazy.spawn("flameshot"), desc="Launch utility for screenshots"),
-
-    # Terminal
-    Key([mod], "t", lazy.spawn(terminal), desc="Launch terminal"),
-
-    # Navigator
-    Key([mod], "f", lazy.spawn("firefox"), desc="Launch firefox"),
-
-    # Communication
-    Key([mod], "m", lazy.spawn("telegram-desktop"), desc="Launch telegram"),
-    Key([mod], "n", lazy.spawn("discord"), desc="Launch discord"),
-
-    # Launcher
-    # Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-    Key([mod], "r", lazy.spawn("rofi -show drun"), desc="Launch rofi"),
-
-    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-]
+keys = keysMapping()
 
 # Drag floating layouts.
 mouse = [
--- .config/qtile/setting/keys.py
+++ .config/qtile/setting/keys.py
@@ -1,78 +1,85 @@
 from libqtile.utils import guess_terminal
-from libqtile.config import Key
-
 from libqtile.command import lazy
+from libqtile.config import Key
+from libqtile.lazy import lazy
 
-mod = "mod4"
+import subprocess
+
+@lazy.function
+def changeTheme(qtile):
+    subprocess.call("/home/andresmpa/.config/qtile/util/change_theme.sh", shell=True)
+
 terminal = guess_terminal()
+mod = "mod4"
 
-keys = [Key(key[0], key[1], *key[2:]) for key in [
-    # Switch between windows
-    ([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    ([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    ([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    ([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    ([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+def keysMapping():
+    return [Key(key[0], key[1], *key[2:]) for key in [
+        ([mod], "u", changeTheme()),
+        # Switch between windows
+        ([mod], "h", lazy.layout.left()),
+        ([mod], "l", lazy.layout.right()),
+        ([mod], "j", lazy.layout.down()),
+        ([mod], "k", lazy.layout.up()),
+        ([mod], "space", lazy.layout.next()),
 
-    # Switch between windows
-    ([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    ([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    ([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    ([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    ([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+        # Switch between windows
+        ([mod], "h", lazy.layout.left()),
+        ([mod], "l", lazy.layout.right()),
+        ([mod], "j", lazy.layout.down()),
+        ([mod], "k", lazy.layout.up()),
+        ([mod], "space", lazy.layout.next()),
 
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    ([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    ([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    ([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    ([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+        # Move windows between left/right columns or move up/down in current stack.
+        # Moving out of range in Columns layout will create new column.
+        ([mod, "shift"], "h", lazy.layout.shuffle_left()),
+        ([mod, "shift"], "l", lazy.layout.shuffle_right()),
+        ([mod, "shift"], "j", lazy.layout.shuffle_down()),
+        ([mod, "shift"], "k", lazy.layout.shuffle_up()),
 
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    ([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    ([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    ([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    ([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    ([mod, "control"], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+        # Grow windows. If current window is on the edge of screen and direction
+        # will be to screen edge - window would shrink.
+        ([mod, "control"], "h", lazy.layout.grow_left()),
+        ([mod, "control"], "l", lazy.layout.grow_right()),
+        ([mod, "control"], "j", lazy.layout.grow_down()),
+        ([mod, "control"], "k", lazy.layout.grow_up()),
+        ([mod, "control"], "n", lazy.layout.normalize()),
 
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    ([mod, "shift"], "Return", lazy.layout.toggle_split(),
-     desc="Toggle between split and unsplit sides of stack"),
+        # Toggle between split and unsplit sides of stack.
+        # Split = all windows displayed
+        # Unsplit = 1 window displayed, like Max layout, but still with
+        # multiple stack panes
+        ([mod, "shift"], "Return", lazy.layout.toggle_split()),
 
-    # Toggle between different layouts as defined below
-    ([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    ([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+        # Toggle between different layouts as defined below
+        ([mod], "Tab", lazy.next_layout()),
+        ([mod], "w", lazy.window.kill()),
 
-    # Volume
-    ([], "XF86AudioLowerVolume", lazy.spawn("pamixer --decrease 5")),
-    ([], "XF86AudioRaiseVolume", lazy.spawn("pamixer --increase 5")),
-    ([], "XF86AudioMute", lazy.spawn("pamixer --toggle-mute")),
+        # Volume
+        ([], "XF86AudioLowerVolume", lazy.spawn("pamixer --decrease 5")),
+        ([], "XF86AudioRaiseVolume", lazy.spawn("pamixer --increase 5")),
+        ([], "XF86AudioMute", lazy.spawn("pamixer --toggle-mute")),
 
-    # Brightness
-    ([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +10%")),
-    ([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10%-")),
+        # Brightness
+        ([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +10%")),
+        ([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10%-")),
 
-    # Print Screen
-    ([], "Print", lazy.spawn("flameshot"), desc="Launch utility for screenshots"),
+        # Print Screen
+        ([], "Print", lazy.spawn("flameshot")),
 
-    # Terminal
-    ([mod], "t", lazy.spawn(terminal), desc="Launch terminal"),
+        # Terminal
+        ([mod], "t", lazy.spawn(terminal)),
 
-    # Navigator
-    ([mod], "f", lazy.spawn("firefox"), desc="Launch firefox"),
+        # Navigator
+        ([mod], "f", lazy.spawn("firefox")),
 
-    # Communication
-    ([mod], "m", lazy.spawn("telegram-desktop"), desc="Launch telegram"),
-    ([mod], "n", lazy.spawn("discord"), desc="Launch discord"),
+        # Communication
+        ([mod], "m", lazy.spawn("telegram-desktop")),
+        ([mod], "n", lazy.spawn("discord")),
 
-    # Launcher
-    # ([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-    ([mod], "r", lazy.spawn("rofi -show drun"), desc="Launch rofi"),
+        # Launcher
+        # ([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+        ([mod], "r", lazy.spawn("rofi -show drun")),
 
-    ([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
-    ([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-]]
+        ([mod, "control"], "r", lazy.restart()),
+        ([mod, "control"], "q", lazy.shutdown()),
+    ]]
