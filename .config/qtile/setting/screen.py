2cb174ff6a6f9d87e236ba079526ebabfe413840
AndresMpa <andresprieto2703@gmail.com>
2022-02-06 01:43:57 -0500

    Splitting my Qtile config

--- .config/qtile/setting/screen.py
+++ .config/qtile/setting/screen.py
@@ -1,9 +1,23 @@
-from libqtile import layout
-from libqtile.config import Match
+# Theme dependencies
+import sys
+import os
 
-from libqtile import bar
-from libqtile import widget
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+from util.color import getTheme
+
+# Qtile dependencies
+
+from libqtile.config import Match
+from libqtile import layout
+
 from libqtile.config import Screen
+from libqtile import widget
+from libqtile import bar
+
+theme = getTheme()
 
 widget_defaults = dict(
     font='sans',
@@ -14,159 +28,179 @@ widget_defaults = dict(
 
 extension_defaults = widget_defaults.copy()
 
-screens = [
-    Screen(
-        bottom=bar.Bar(
-            [
-                widget.TextBox(
-                    "",
-                    background="#360054",
-                    foreground="#ffac34",
-                    fontsize=32,
-                    margin=-2,
-                    padding=4
-                ),
-                widget.TextBox(
-                    "",
-                    background="#16003e",
-                    foreground="#360054",
-                    fontsize=24,
-                    padding=-2
-                ),
-                widget.GroupBox(
-                    padding=3,
-                    fontsize=16,
-                    active="#ffac34",
-                    inactive="#0ebefe",
-                    background="#16003e",
-                    this_current_screen_border="#155089",
-                ),
-                widget.Moc(
-                    background="#16003e",
-                ),
-                widget.TextBox(
-                    "",
-                    foreground="#16003e",
-                    fontsize=24,
-                    padding=-2
-                ),
+def getScreen():
+    return [
+        Screen(
+            bottom=bar.Bar(
+                [
+                    # Icon
+                    widget.TextBox(
+                        "",
+                        background=theme["icon"][0],
+                        foreground=theme["icon"][1],
+                        fontsize=32,
+                        margin=-2,
+                        padding=4
+                    ),
+                    widget.TextBox(
+                        "",
+                        background=theme["icon"][2],
+                        foreground=theme["icon"][3],
+                        fontsize=24,
+                        padding=-2
+                    ),
 
-                widget.Spacer(),
 
-                widget.TextBox(
-                    " ",
-                    foreground="#360054",
-                    fontsize=24,
-                    padding=-10
-                ),
-                widget.Backlight(
-                    fmt=" {}",
-                    background="#360054",
-                    foreground="#0ebefe",
-                    backlight_name="amdgpu_bl0",
-                    change_command="brightnessctl set {0}%",
-                    brightness_file="/sys/class/backlight/amdgpu_bl0/brightness",
-                    max_brightness_file="/sys/class/backlight/amdgpu_bl0/max_brightness",
-                ),
-                widget.TextBox(
-                    "",
-                    background="#360054",
-                    foreground="#360054",
-                    fontsize=24,
-                    padding=-10
-                ),
-                widget.Volume(
-                    fmt=" {}",
-                    background="#360054",
-                    foreground="#0ebefe",
-                ),
-                widget.TextBox(
-                    "",
-                    background="#360054",
-                    foreground="#360054",
-                    fontsize=24,
-                    padding=-10
-                ),
-                widget.Battery(
-                    background="#360054",
-                    foreground="#0ebefe",
-                    low_foreground="#ffac34",
-                    format=" {percent:2.0%}",
-                ),
-                widget.TextBox(
-                    "",
-                    background="#360054",
-                    foreground="#360054",
-                    fontsize=24,
-                    padding=-10
-                ),
-                widget.ThermalSensor(
-                    background="#360054",
-                    foreground="#0ebefe",
-                    fmt=" {}",
-                ),
-                widget.TextBox(
-                    "",
-                    foreground="#360054",
-                    fontsize=24,
-                    padding=-2,
-                    margin=0
-                ),
-                widget.Spacer(),
-                widget.TextBox(
-                    " ",
-                    foreground="#16003e",
-                    fontsize=24,
-                    padding=-10
-                ),
-                widget.Clipboard(
-                    background="#16003e",
-                    foreground="#0ebefe",
-                ),
-                widget.Systray(
-                    background="#16003e",
-                    foreground="#0ebefe",
-                    padding=3,
-                ),
-                widget.CheckUpdates(
-                    background="#16003e",
-                    foreground="#0ebefe",
-                    colour_have_updates="#0ebefe",
-                    restart_indicator="",
-                    no_update_string="﫟",
-                    display_format="",
-                    fontsize=23,
-                    padding=4,
-                ),
-                widget.CurrentLayoutIcon(
-                    background="#16003e",
-                    foreground="#0ebefe",
-                    padding=9,
-                ),
-                widget.Clock(
-                    background="#16003e",
-                    foreground="#0ebefe",
-                    format='%a %d/%m/%Y  %I:%M %p'
-                ),
-                widget.TextBox(
-                    " ",
-                    background="#16003e",
-                    foreground="#360054",
-                    fontsize=24,
-                    padding=-10
-                ),
-                widget.QuickExit(
-                    background="#360054",
-                    foreground="#0ebefe",
-                    countdown_format="{}",
-                    default_text="  ",
-                    fontsize=20
-                ),
-            ],
-            24,
-            margin=5,
-            opacity=1,
-            background='#16003e',
+                    # Desk
+                    widget.GroupBox(
+                        padding=3,
+                        fontsize=16,
+                        active=theme["desk"][0],
+                        inactive=theme["desk"][1],
+                        background=theme["desk"][2],
+                        this_current_screen_border=theme["desk"][3],
+                    ),
+                    widget.Moc(
+                        background=theme["desk"][4],
+                    ),
+                    widget.TextBox(
+                        "",
+                        foreground=theme["desk"][2],
+                        background=theme["desk"][4],
+                        fontsize=24,
+                        padding=-2
+                    ),
+
+                    widget.Spacer(),
+
+                    widget.TextBox(
+                        " ",
+                        foreground=theme["manager"][0],
+                        fontsize=24,
+                        padding=-10
+                    ),
+                    widget.Backlight(
+                        fmt=" {}",
+                        background=theme["manager"][0],
+                        foreground=theme["manager"][1],
+                        backlight_name="amdgpu_bl0",
+                        change_command="brightnessctl set {0}%",
+                        brightness_file="/sys/class/backlight/amdgpu_bl0/brightness",
+                        max_brightness_file="/sys/class/backlight/amdgpu_bl0/max_brightness",
+                    ),
+                    widget.TextBox(
+                        "",
+                        background=theme["manager"][0],
+                        foreground=theme["manager"][0],
+                        fontsize=24,
+                        padding=-10
+                    ),
+                    widget.Volume(
+                        fmt=" {}",
+                        background=theme["manager"][0],
+                        foreground=theme["manager"][1],
+                    ),
+                    widget.TextBox(
+                        "",
+                        background=theme["manager"][0],
+                        foreground=theme["manager"][0],
+                        fontsize=24,
+                        padding=-10
+                    ),
+                    widget.Battery(
+                        background=theme["manager"][0],
+                        foreground=theme["manager"][1],
+                        low_foreground=theme["manager"][2],
+                        full_char="",
+                        empty_char="",
+                        charge_char="",
+                        discharge_char="",
+                        # hide_threshold=0.9,
+                        show_short_text=False,
+                        format="{char} {percent:2.0%}",
+                    ),
+                    widget.TextBox(
+                        "",
+                        background=theme["manager"][0],
+                        foreground=theme["manager"][0],
+                        fontsize=24,
+                        padding=-10
+                    ),
+                    widget.ThermalSensor(
+                        background=theme["manager"][0],
+                        foreground=theme["manager"][1],
+                        fmt=" {}",
+                    ),
+                    widget.TextBox(
+                        "",
+                        foreground=theme["manager"][0],
+                        fontsize=24,
+                        padding=-2,
+                        margin=0
+                    ),
+
+                    widget.Spacer(),
+
+                    widget.TextBox(
+                        " ",
+                        foreground=theme["util"][0],
+                        fontsize=24,
+                        padding=-10
+                    ),
+                    widget.Clipboard(
+                        background=theme["util"][0],
+                        foreground=theme["util"][1],
+                    ),
+                    widget.Systray(
+                        background=theme["util"][0],
+                        foreground=theme["util"][1],
+                        padding=3,
+                    ),
+                    widget.CheckUpdates(
+                        background=theme["util"][0],
+                        foreground=theme["util"][1],
+                        colour_have_updates=theme["util"][1],
+                        restart_indicator="",
+                        no_update_string="﫟",
+                        display_format="",
+                        fontsize=23,
+                        padding=4,
+                    ),
+                    widget.CurrentLayoutIcon(
+                        background=theme["util"][0],
+                        foreground=theme["util"][1],
+                        padding=9,
+                    ),
+                    widget.Clock(
+                        background=theme["util"][0],
+                        foreground=theme["util"][1],
+                        format='%a %d/%m/%Y  %I:%M %p'
+                    ),
+                    widget.TextBox(
+                        " ",
+                        background=theme["icon"][2],
+                        foreground=theme["icon"][0],
+                        fontsize=24,
+                        padding=-10
+                    ),
+                    widget.QuickExit(
+                        background=theme["icon"][0],
+                        foreground=theme["icon"][1],
+                        countdown_format="{}",
+                        default_text=" ",
+                        fontsize=20
+                    ),
+                ],
+                24,
+                margin=5,
+                opacity=1,
+                background=theme["bar"],
+            ),
         ),
-    ),
-]
+    ]
+
+# widget.Prompt(),
+# widget.WindowName(),
+# widget.ThermalSensor(),
+
+#    
--- .config/qtile/util/change_theme.sh
+++ .config/qtile/util/change_theme.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-theme=($(ls --hide="more" ~/Wallpapers/))
-current=$(cat ~/.config/qtile/current.txt)
+theme=($(ls --hide="more" /home/andresmpa/Wallpapers/))
+current=$(cat /home/andresmpa/.config/qtile/current.txt)
 len="${#theme[@]}"
 
 for ((option = 0; option < $len; option++)); do
 	if [[ ${theme[$option]%%.*} == $current ]]; then
 		if [[ $option -eq $(($len - 1)) ]]; then
 			echo "Changing to ${theme[0]%%.*}"
-			feh --bg-scale ~/Wallpapers/${theme[0]}
-			sed -i "s/$current/${theme[0]%%.*}/" ~/.config/qtile/current.txt
+			feh --bg-scale /home/andresmpa/Wallpapers/${theme[0]}
+			sed -i "s/$current/${theme[0]%%.*}/" /home/andresmpa/.config/qtile/current.txt
 		else
 			echo "Changing to ${theme[$option + 1]%%.*}"
-			feh --bg-scale ~/Wallpapers/${theme[$option + 1]}
-			sed -i "s/$current/${theme[$option + 1]%%.*}/" ~/.config/qtile/current.txt
+			feh --bg-scale /home/andresmpa/Wallpapers/${theme[$option + 1]}
+			sed -i "s/$current/${theme[$option + 1]%%.*}/" /home/andresmpa/.config/qtile/current.txt
 		fi
 	fi
 done
