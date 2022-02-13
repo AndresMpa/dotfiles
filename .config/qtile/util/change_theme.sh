8228c9b4e7afa950ce2d1f9746e29bead244dcc1
AndresMpa <andresprieto2703@gmail.com>
2022-02-12 23:11:18 -0500

    KISS
    Making it simpler

--- .config/qtile/util/change_theme.sh
+++ .config/qtile/util/change_theme.sh
@@ -6,13 +6,16 @@ len="${#theme[@]}"
 
 for ((option = 0; option < $len; option++)); do
 	if [[ ${theme[$option]%%.*} == $current ]]; then
+		rm /home/andresmpa/Wallpapers/current/*
 		if [[ $option -eq $(($len - 1)) ]]; then
 			echo "Changing to ${theme[0]%%.*}"
 			feh --bg-scale /home/andresmpa/Wallpapers/${theme[0]}
+			cp /home/andresmpa/Wallpapers/${theme[0]} /home/andresmpa/Wallpapers/current/
 			sed -i "s/$current/${theme[0]%%.*}/" /home/andresmpa/.config/qtile/current.txt
 		else
 			echo "Changing to ${theme[$option + 1]%%.*}"
 			feh --bg-scale /home/andresmpa/Wallpapers/${theme[$option + 1]}
+			cp /home/andresmpa/Wallpapers/${theme[$option + 1]} /home/andresmpa/Wallpapers/current/
 			sed -i "s/$current/${theme[$option + 1]%%.*}/" /home/andresmpa/.config/qtile/current.txt
 		fi
 	fi
