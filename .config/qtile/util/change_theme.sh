4923f4fad9584c318a74d5d627d9a17c3c2e1af4
AndresMpa <andresprieto2703@gmail.com>
2022-02-13 01:23:49 -0500

    Qick fix

--- .config/qtile/util/change_theme.sh
+++ .config/qtile/util/change_theme.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+oldWallpaper=($(ls --hide="more" /home/andresmpa/Wallpapers/current/))
 theme=($(ls --hide="more" /home/andresmpa/Wallpapers/))
 current=$(cat /home/andresmpa/.config/qtile/current.txt)
 len="${#theme[@]}"
 
 for ((option = 0; option < $len; option++)); do
 	if [[ ${theme[$option]%%.*} == $current ]]; then
-		rm /home/andresmpa/Wallpapers/current/*
+		rm /home/andresmpa/Wallpapers/current/$oldWallpaper
 		if [[ $option -eq $(($len - 1)) ]]; then
 			echo "Changing to ${theme[0]%%.*}"
 			feh --bg-scale /home/andresmpa/Wallpapers/${theme[0]}
