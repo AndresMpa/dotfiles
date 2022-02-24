2fcfc20b71d21d86532c2441453806f9a0dcc061
Andres M. Prieto <andresprieto2703@gmail.com>
2022-02-23 23:20:35 -0500

    Adding logs

--- .config/qtileUpdate.sh
+++ .config/qtileUpdate.sh
@@ -3,11 +3,64 @@
 BASE="/home/$USER/Escritorio/dotfiles/.config"
 
 # Removing old configurations
-rm -rf $BASE/kitty/ $BASE/qtile/ $BASE/nitrogen/ $BASE/picom/ $BASE/rofi/
+{
+	rm -rf $BASE/qtile/
+} || {
+	echo "Error removing" >>failures.log
+}
+{
+	rm -rf $BASE/kitty/
+} || {
+	echo "Error removing" >>failures.log
+}
+{
+	rm -rf $BASE/nitrogen/
+} || {
+	echo "Error removing" >>failures.log
+}
+{
+	rm -rf $BASE/picom/
+} || {
+	echo "Error removing" >>failures.log
+}
+{
+	rm -rf $BASE/rofi/
+} || {
+	echo "Error removing" >>failures.log
+}
 
 # Updating pacman configurations
 rm -rf $BASE/pacman.conf
 cat /etc/pacman.conf >$BASE/pacman.conf
 
 # Configuration folders
-cp -r ~/.config/kitty ~/.config/qtile ~/.config/nitrogen/ ~/.config/picom ~/.config/rofi ~/.config/scripts $BASE/
+{
+	cp -r ~/.config/qtile $BASE/
+} || {
+	echo "Error updating qtile" >>failures.log
+}
+{
+	cp -r ~/.config/kitty $BASE/
+} || {
+	echo "Error updating kitty" >>failures.log
+}
+{
+	cp -r ~/.config/nitrogen/ $BASE/
+} || {
+	echo "Error updating nitrogen" >>failures.log
+}
+{
+	cp -r ~/.config/picom $BASE/
+} || {
+	echo "Error updating picom" >>failures.log
+}
+{
+	cp -r ~/.config/rofi $BASE/
+} || {
+	echo "Error updating rofi" >>failures.log
+}
+{
+	cp -r ~/.config/scripts $BASE/
+} || {
+	echo "Error updating scripts" >>failures.log
+}
