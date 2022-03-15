b1b8a71a6ec1f0f2e4b5f07527e5ca2692b0b3ca
Andres M. Prieto <andresprieto2703@gmail.com>
2022-03-15 17:23:47 -0500

    THe f*** CIRCUMFLEX
    I got the circumflex working under latam layout

--- AndresMpa/keymap.c
+++ AndresMpa/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                 ,-----------------------------------------------------.
     KC_PPLS, KC_PMNS,  ES_1,    ES_2,    ES_3,    KC_PEQL,                    KC_VOLD, KC_VOLU, KC_MUTE, KC_BRIU, KC_BRID, KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                 |--------+--------+--------+--------+--------+--------|
-    KC_PAST, KC_PSLS,  ES_4,    ES_5,    ES_6,    ES_LCBR,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_PSCR, KC_ESC,
+    KC_PAST, KC_PSLS,  ES_4,    ES_5,    ES_6,    ES_LCBR,                    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_PSCR, KC_ESC,
   //|--------+--------+--------+--------+--------+--------|                 |--------+--------+--------+--------+--------+--------|
     ES_LABK, ES_RABK,  ES_7,    ES_8,    ES_9,    ES_0,                      KC_CUT,  KC_COPY, KC_PSTE, KC_UNDO, KC_FIND, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+------|  |-------+--------+--------+--------+--------+--------+--------|
@@ -45,13 +45,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`------------------------'  `--------------------------'
   ),
 
-  // \\ ~~~
   // KC_LCBR = ¨
   [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                ,-----------------------------------------------------.
-    KC_TAB,  ES_AT,   KC_HASH, ES_DLR,  ES_AMPR, ES_PIPE,                   ES_QUOT, KC_ASTR, KC_LPRN, ES_GRV,  XXXXXXX, KC_BSPC,
+    KC_TAB,  KC_HASH, ES_AT,   ES_DLR,  ES_AMPR, ES_PIPE,                   ES_QUOT, KC_ASTR, KC_LPRN, ES_GRV,  XXXXXXX, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                |--------+--------+--------+--------+--------+--------|
-    KC_PERC, ES_BSLS, ES_TILD, KC_TILD, ES_IEXL, ES_QUES,                   ES_DQUO, KC_QUOT, KC_BSLS, ES_RCBR, XXXXXXX, XXXXXXX,
+    ES_CIRC, KC_PERC, KC_TILD, ES_TILD, ES_IEXL, ES_QUES,                   ES_DQUO, KC_QUOT, KC_BSLS, ES_RCBR, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                |--------+--------+--------+--------+--------+--------|
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, ES_IQUE, ES_EXLM,                   XXXXXXX, ES_DIAE, KC_PIPE, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+------|  |------+--------+--------+--------+--------+--------+--------|
