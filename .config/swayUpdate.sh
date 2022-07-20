06e6c8ff3c6833449ff72a56f32228ba0ab5aa65
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-20 00:50:35 -0500

    Automating updates
    Updating update script

---
+++ .config/swayUpdate.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+BASE="/home/$USER/Escritorio/dotfiles/.config"
+
+# Removing old configurations
+{
+	rm -rf $BASE/sway
+} || {
+	echo "Error removing sway" >>failures.log
+}
+{
+	rm -rf $BASE/waybar
+} || {
+	echo "Error removing waybar" >>failures.log
+}
+{
+	rm -rf $BASE/wofi
+} || {
+	echo "Error removing wofi" >>failures.log
+}
+{
+	rm -rf $BASE/wezterm
+} || {
+	echo "Error removing wezterm" >>failures.log
+}
+
+# Updating pacman configurations
+rm -rf $BASE/pacman.conf
+cat /etc/pacman.conf >$BASE/pacman.conf
+
+# Configuration folders
+{
+	cp -r ~/.config/sway $BASE/
+} || {
+	echo "Error updating sway" >>failures.log
+}
+{
+	cp -r ~/.config/waybar $BASE/
+} || {
+	echo "Error updating waybar" >>failures.log
+}
+{
+	cp -r ~/.config/wofi $BASE/
+} || {
+	echo "Error updating wofi" >>failures.log
+}
+{
+	cp -r ~/.config/wezterm $BASE/
+} || {
+	echo "Error updating wezterm" >>failures.log
+}
