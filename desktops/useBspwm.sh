2f628ce43e96a72fba1bf198dd238124a1b7d1c5
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-20 23:30:07 -0500

    MLD
    Making less dump my installation file

--- desktops/useBspwm.sh
+++ desktops/useBspwm.sh
@@ -19,53 +19,7 @@ sudo pacman -S --noconfirm alacritty bspwm nitrogen picom polybar rofi sxhkd
 	echo "Error moving files"
 }
 
-echo -n "Do you want to install oh-my-zsh? [y/n]: "
-read zsh
-
-if [[ $zsh == "y" ]];
-then
-
-	echo "Package managers"
-	sudo pacman -S nodejs npm pip python --noconfirm
-
-	wget -P ~/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-	wget -P ~/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-	wget -P ~/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-	wget -P ~/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
-	fc-cache -f -v
-
-	# Installing oh-my-zsh
-	sudo pacman -S zsh fzf neofetch --noconfirm
-	rm -rf /home/$USER/.oh-my-zsh
-	wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-	chsh -s `which zsh`
-
-	# Adding zsh default configuration
-	cat zsh_configuration > ~/.zshrc 
-
-	# Powerlevel10k
-	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-	
-	# Commands highlighting
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-	
-	# Auto suggestions
-	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-	
-	# Auto complete
-	git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
-	cd ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
-	source zsh-autocomplete.plugin.zsh
-
-	cp ./.zshrc > ~/.zshrc
-
-	cp $(pwd)/fonts/* ~/.local/share/fonts
-fi
-
-cat ./.xinitrc > ~/.xinitrc
-
-sudo cat ./.config/pacman.conf > /etc/pacman.conf
-
+# Hardware
 echo -n "Do you use amdgpu_bl0? [y/n]: "
 read brightness
 
@@ -73,3 +27,16 @@ if [[ $brightness == "y" ]];
 then
 	cp ./.config/brightness-permission.service > /etc/systemd/system/brightness-permission.service
 fi
+
+cp ../Wallpapers ~/
+
+cat ../.xinitrc > ~/.xinitrc
+echo "feh --bg-scale /home/$USER/Wallpapers/"
+echo "exec bspwm" >> ~/.xinitrc
+echo "exec sxhkd" >> ~/.xinitrc
+
+sudo cat ./.config/pacman.conf > /etc/pacman.conf
+
+# Shell
+git clone https://github.com/AndresMpa/desl/ 
+cd desl && ./install.sh
