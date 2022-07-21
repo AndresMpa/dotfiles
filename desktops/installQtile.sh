2b9f6287d78dafa24e679de33b3ecb5e10f0a28d
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-20 23:57:35 -0500

    Qtile
    Completting Qtile installation files

--- desktops/installQtile.sh
+++ desktops/installQtile.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "Working on it"
+echo "Qtile from scratch"
+sudo pacman -Syu --noconfirm
+sudo pacman --noconfirm -S qtile kitty picom rofi
+
+# Utilities
+sudo pacman --noconfirm -S firefox dunst feh
+
+cp ../Wallpapers ~/
+
+cat ../.xinitrc > ~/.xinitrc
+echo "feh --bg-scale /home/$USER/Wallpapers/current/*" >> ~/.xinitrc
+echo "exec qtile start" >> ~/.xinitrc
--- desktops/useQtile.sh
+++ desktops/useQtile.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo "Working on it"
+echo "Updating"
+sudo pacman -Syyu --noconfirm
+echo "Installing dependencies"
+sudo pacman --noconfirm -S qtile kitty picom rofi
+{
+	cp ../.config/scripts > ~/.config/scripts 
+	cp ../.config/qtile > ~/.config/qtile
+	cp ../.config/kitty > ~/.config/kitty
+	cp ../.config/picom > ~/.config/picom
+	cp ../.config/rofi > ~/.config/rofi
+} || {
+	echo "Error moving files"
+}
+
+# Utilities
+sudo pacman --noconfirm -S firefox dunst feh
+
+sudo cat ./.config/pacman.conf > /etc/pacman.conf
