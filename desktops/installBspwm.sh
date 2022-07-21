56ffd0191141ed26896afe4d3e562da4efa13b11
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-20 23:51:21 -0500

    Refactor
    Using single responsability

--- desktops/installBspwm.sh
+++ desktops/installBspwm.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-echo "All you need to make this thing work is the next"
-
-echo "BSPWM"
-sudo pacman --noconfirm -Syu libxcb xcb-util xcb-util-wm xcb-util-keysyms
-git clone https://github.com/baskerville/bspwm.git
-git clone https://github.com/baskerville/sxhkd.git
-cd bspwm && make && sudo make install
-cd ../sxhkd && make && sudo make install
-cd
+echo "BSPWM from scratch"
+sudo pacman -Syu --noconfirm
+sudo pacman --noconfirm -S libxcb xcb-util xcb-util-wm xcb-util-keysyms
+sudo pacman --noconfirm -S bspwm sxhkd
 
 mkdir -p ~/.config/{bspwm,sxhkd}
 cp /usr/local/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
@@ -21,23 +16,3 @@ sudo pacman --noconfirm -S polybar
 
 # Utilities
 sudo pacman --noconfirm -S alacritty firefox rofi feh
-
-# Hardware
-echo -n "Do you use amdgpu_bl0? [y/n]: "
-read brightness
-
-if [[ $brightness == "y" ]];
-then
-	cp ./.config/brightness-permission.service > /etc/systemd/system/brightness-permission.service
-fi
-
-cp ../Wallpapers ~/
-
-cat ../.xinitrc > ~/.xinitrc
-echo "feh --bg-scale /home/$USER/Wallpapers/"
-echo "exec bspwm" >> ~/.xinitrc
-echo "exec sxhkd" >> ~/.xinitrc
-
-# Shell
-git clone https://github.com/AndresMpa/desl/ 
-cd desl && ./install.sh
