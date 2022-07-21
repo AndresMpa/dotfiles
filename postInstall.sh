813b1d14922e92e18f16706e26a6f79a457ca46b
Andres M. Prieto <andresprieto2703@gmail.com>
2022-07-20 23:58:54 -0500

    TODO
    Updating task, also using a file to handle post Install

---
+++ postInstall.sh
@@ -0,0 +1,12 @@
+# Hardware
+echo -n "Do you use amdgpu_bl0? [y/n]: "
+read brightness
+
+if [[ $brightness == "y" ]];
+then
+	cp ./.config/brightness-permission.service > /etc/systemd/system/brightness-permission.service
+fi
+
+# Shell
+git clone https://github.com/AndresMpa/desl/ 
+cd desl && ./install.sh
