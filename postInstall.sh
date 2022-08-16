4f3047b7d8f6c0085e15c45945897423f103d050
Andres M. Prieto <andresprieto2703@gmail.com>
2022-08-15 22:52:12 -0500

    Minor changes
    Some identation fixes

--- postInstall.sh
+++ postInstall.sh
@@ -2,11 +2,10 @@
 echo -n "Do you use amdgpu_bl0? [y/n]: "
 read brightness
 
-if [[ $brightness == "y" ]];
-then
-	cp ./.config/brightness-permission.service > /etc/systemd/system/brightness-permission.service
+if [[ $brightness == "y" ]]; then
+	cp ./.config/brightness-permission.service >/etc/systemd/system/brightness-permission.service
 fi
 
 # Shell
-git clone https://github.com/AndresMpa/desl/ 
+git clone https://github.com/AndresMpa/desl/
 cd desl && ./install.sh
