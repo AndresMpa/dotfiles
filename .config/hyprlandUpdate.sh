f74e3898534d6affb8aa8836c5cbd1e8696aefa8
Andres M. Prieto <andresprieto2703@gmail.com>
2022-12-05 01:46:05 +0000

    Hyprland
    Adding Hyprland small configurations, I did not like it sorry

---
+++ .config/hyprlandUpdate.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+BASE="/$(pwd)/.config"
+
+# Removing old configurations
+{
+	rm -rf $BASE/hypr
+} || {
+	echo "Error removing hypr" >>failures.log
+}
+{
+	rm -rf $BASE/waybar
+} || {
+	echo "Error removing waybar" >>failures.log
+}
+{
+	rm -rf $BASE/wofi
+} || {
+	echo "Error removing wofi" >>failures.log
+}
+{
+	rm -rf $BASE/kitty
+} || {
+	echo "Error removing kitty" >>failures.log
+}
+{
+	rm -rf $BASE/mako
+} || {
+	echo "Error removing mako" >>failures.log
+}
+
+# Updating pacman configurations
+rm -rf $BASE/pacman.conf
+cat /etc/pacman.conf >$BASE/pacman.conf
+
+# Configuration folders
+{
+	cp -r ~/.config/hypr $BASE/
+} || {
+	echo "Error updating hypr" >>failures.log
+}
+{
+	cp -r ~/.config/waybar $BASE/
+} || {
+	echo "Error updating waybar" >>failures.log
+}
+{
+	cp -r ~/.config/wofi $BASE/
+} || {
+	echo "Error updating wofi" >>failures.log
+}
+{
+	cp -r ~/.config/kitty $BASE/
+} || {
+	echo "Error updating kitty" >>failures.log
+}
+{
+	cp -r ~/.config/mako $BASE/
+} || {
+	echo "Error updating mako" >>failures.log
+}
