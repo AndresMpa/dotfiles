b5c00c60d0a22213231c55dd7d5bc58c105728cd
Andres M. Prieto <andresprieto2703@gmail.com>
2023-01-18 13:50:16 -0500

    Multi logo
    Using multiples logos on slave keyboard

--- AndresMpa/config.h
+++ AndresMpa/config.h
@@ -30,6 +30,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL_PD2
 
+#define SPLIT_LAYER_STATE_ENABLE
+
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 100
 
