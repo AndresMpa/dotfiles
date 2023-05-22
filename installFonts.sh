e72b5464614f59a765656d69fca975fca343d8a3
Andres M. Prieto <andresprieto2703@gmail.com>
2023-05-22 06:05:19 -0500

    Small fix
    Moving confirmation upper to make it more useful

--- installFonts.sh
+++ installFonts.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z ~/.local/share/fonts ];
+then
+	mkdir ~/.local/share/fonts
+fi
+
 cp ./fonts/* ~/.local/share/fonts/
 
 wget https://github.com/google/fonts/archive/main.zip
@@ -9,11 +14,6 @@ unzip main.zip
 cp $0 main
 
 ttfFile="$(find ./ -type f -name '*.ttf')"
-
-if [ -z ~/.local/share/fonts ];
-then
-	mkdir ~/.local/share/fonts
-fi
 
 for file in $ttfFile; do
 	cp $file ~/.local/share/fonts/
