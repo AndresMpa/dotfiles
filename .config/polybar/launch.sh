1c1a1856dd40bcc0b685d5f1082b1b346e23e462
Andres M. Prieto <andresprieto2703@gmail.com>
2023-06-14 05:02:20 -0500

    Multi bars
    Using multiple bars for multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,6 +8,17 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar -r -c $HOME/.config/polybar/config.ini top_bar &
-#polybar -r -c $HOME/.config/polybar/config.ini bottom_bar &
+
+if type "xrandr"; then
+  for monitors in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    if [[ $monitors == *"HDMI"* ]]; then
+      MONITOR=$monitors polybar --reload extra_top_bar &
+    else
+      MONITOR=$monitors polybar --reload top_bar &
+    fi
+  done
+else
+  polybar --reload top_bar &
+fi
 
 echo "Polybar launched"
